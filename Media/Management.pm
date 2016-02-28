c16f9d47edd1563c4d9f9a5090e2cec1bfeaee13
Tony Snearly <tony.snearly@gmail.com>
2016-02-28 17:02:17 -0600

    Create Management.pm

---
+++ Media/Management.pm
@@ -0,0 +1,11 @@
+package Media::Management;
+
+# Please, if you tested it in some earlier version of Perl and works let
+# me know! The versions of Filter::Simple, Text::Tabs, and Test::More
+# would be useful as well.
+use 5.006_001;
+use strict;
+use warnings;
+
+our ($VERSION, $DEBUG, $CALLER);
+$VERSION = '0.47';
